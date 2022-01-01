81808e9b7d777a3620a5b5945ee75b9e51e9253f
Jonas Kaendler <jonas.kaendler@valueworks.ai>
2022-01-01 17:58:24 +0100

    deleted frontend as we will put it in a separate repo

--- frontend/src/components/Playlist.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	// create a prop
-	export let playlist;
-</script>
-
-<article>
-	<h1>{playlist.name}</h1>
-	<img src={playlist.image} alt={playlist.name} width="274" height="274" />
-</article>
